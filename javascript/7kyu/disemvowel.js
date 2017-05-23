a9adf63a6e4361900c64acd154d0b87a84765bfd
chavocado <d.chavez562@yahoo.com>
2017-05-23 10:17:51 -0500

    commenting, and added disemvowel

---
+++ javascript/7kyu/disemvowel.js
@@ -0,0 +1,4 @@
+function disemvowel(str) {
+  //g = global so entire string; i = case insesnsitive
+  return str.replace(/[aeiou]/gi, '');
+}
--- javascript/7kyu/vowelcount.js
+++ javascript/7kyu/vowelcount.js
@@ -1,5 +1,5 @@
 function getCount(str) {
+  //g = global so entire string; i = case insesnsitive
   var vowelsCount = str.match(/[aeiou]/gi);
-
   return vowelsCount === null ? 0 : vowelsCount.length;
 }
