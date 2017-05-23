176d6366bf80f71ffa82482dd3ceecee043aaca6
chavocado <d.chavez562@yahoo.com>
2017-05-23 11:39:27 -0500

    added READ.ME and commented older solutions for readability

--- javascript/7kyu/vowelcount.js
+++ javascript/7kyu/vowelcount.js
@@ -1,5 +1,6 @@
 function getCount(str) {
   //g = global so entire string; i = case insesnsitive
   var vowelsCount = str.match(/[aeiou]/gi);
+  //ternary condition if vowelsCount === null the value will be 0 or the length
   return vowelsCount === null ? 0 : vowelsCount.length;
 }
--- javascript/8kyu/powersoftwo.js
+++ javascript/8kyu/powersoftwo.js
@@ -1,5 +1,6 @@
 function powersOfTwo(n){
   var expArray = [];
+  //loop through param
   for(var i=0; i<=n; i++) {
   //Math.pow returns base to the ^exp
   expArray.push(Math.pow(2,i));
